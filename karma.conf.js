d9ccce0d7dda1acffcf79c72cc3f26cd2a56fd45
Dominik Duda <dominik.duda6@gmail.com>
2021-05-21 18:40:59 +0200

    list/view/service added

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ebconttask'),
+      dir: require('path').join(__dirname, './coverage/dominikdex'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        HttpClientTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +28,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ebconttask'`, () => {
+  it(`should have as title 'Dominiks Pokedex'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ebconttask');
+    expect(app.title).toEqual('Dominiks Pokedex');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ebconttask app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Pokedex');
   });
 });
